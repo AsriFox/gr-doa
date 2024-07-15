c7f19f5c333887dc3e738cdcbda5064c86c3cf46
AsriFox <asrifox@yandex.ru>
2024-07-15 18:12:35 +0300

    Change qa_find_local_max.py to numpy

--- python/qa_find_local_max.py
+++ python/qa_find_local_max.py
@@ -24,10 +24,20 @@
 from gnuradio import gr, gr_unittest
 from gnuradio import blocks
 import doa_swig as doa
-import oct2py
-import numpy
+import numpy as np
+from numpy import array, pi, sin, cos
+from scipy.signal import find_peaks
 import os
-from subprocess import Popen, PIPE
+
+
+def findpeaks(num_max_vals: int, y: np.ndarray):
+	data = np.abs(y)
+	all_pks_idx, all_pks_props = find_peaks(data, height=0.0)
+	all_pks_idx = np.argsort(-all_pks_props['peak_height'])
+	pks_idx = all_pks_idx[0:num_max_vals]
+	pks = data[pks_idx]
+	return array(pks), array(pks_idx)
+
 
 class qa_find_local_max (gr_unittest.TestCase):
 
@@ -41,11 +51,12 @@ class qa_find_local_max (gr_unittest.TestCase):
 		self.vector_len = 2**11
 		self.num_max_vals = 3
 
-		# generate vector from octave
-		oc = oct2py.Oct2Py()
-		oc.addpath(os.path.dirname(__file__))	
-		data, expected_pks, expected_t_pks = oc.test001_findpeaks(self.num_max_vals, self.vector_len)
-		data = data.flatten().tolist()
+		# generate vector
+		t = 2.0 * pi * np.linspace(0, 1, self.vector_len)
+		y = sin(3.14 * t) + 0.5 * cos(6.09 * t) + 0.1 * sin(10.11 * t + 1 / 6) + 0.1 * sin(15.3 * t + 1 / 3)
+		data = y.flatten().tolist()
+		expected_pks, expected_pks_locs = findpeaks(self.num_max_vals, y)
+		expected_t_pks = t[expected_pks_locs]
 
 		##################################################
 		# Blocks
@@ -66,22 +77,26 @@ class qa_find_local_max (gr_unittest.TestCase):
 		self.tb.run ()
 
 		# get data from sink
-		measured_pks = self.blocks_vector_sink_x_0.data()  
-		measured_pks_locs = self.blocks_vector_sink_x_1.data()  
+		measured_pks = array(self.blocks_vector_sink_x_0.data())
+		measured_pks_locs = array([int(p) for p in self.blocks_vector_sink_x_1.data()])
+		measured_t_pks = t[measured_pks_locs]
 
 		# check data
-		for i in range(len(measured_pks)):
-			self.assertAlmostEqual(expected_pks[i], measured_pks[i], 5) and self.assertAlmostEqual(expected_t_pks[i], measured_t_pks[i], 5)
+		if np.any(np.abs(expected_pks - measured_pks) > 1e-5):
+			self.fail("Expected peak heights are not equal to measured peak heights")
+		if np.any(np.abs(expected_t_pks - measured_t_pks) > 1e-5):
+			self.fail("Expected peak locations are not equal to measured peak locations")
 
 	def test_002_t (self):
 		self.vector_len = 2**12
 		self.num_max_vals = 5
 
-		# generate vector from octave
-		oc = oct2py.Oct2Py()
-		oc.addpath(os.path.dirname(__file__))
-		data, expected_pks, expected_t_pks = oc.test002_findpeaks(self.num_max_vals, self.vector_len)
-		data = data.flatten().tolist()
+		# generate vector
+		t = 2.0 * pi * np.linspace(0, 1, self.vector_len)
+		y = sin(0.25 * 3.14 * t) + 5 * cos(6.09 * t) + 0.6 * sin(1.11 * t + 1 / 6) + 2 * sin(5.3 * t + 1 / 3)
+		data = y.flatten().tolist()
+		expected_pks, expected_pks_locs = findpeaks(self.num_max_vals, y)
+		expected_t_pks = t[expected_pks_locs]
 
 		##################################################
 		# Blocks
@@ -102,12 +117,16 @@ class qa_find_local_max (gr_unittest.TestCase):
 		self.tb.run ()
 
 		# get data from sink
-		measured_pks = self.blocks_vector_sink_x_0.data()  
-		measured_pks_locs = self.blocks_vector_sink_x_1.data()  	
+		measured_pks = array(self.blocks_vector_sink_x_0.data())
+		measured_pks_locs = array([int(p) for p in self.blocks_vector_sink_x_1.data()])
+		measured_t_pks = t[measured_pks_locs]
 
 		# check data
-		for i in range(len(measured_pks)):
-			self.assertAlmostEqual(expected_pks[i], measured_pks[i], 5) and self.assertAlmostEqual(expected_t_pks[i], measured_t_pks[i], 5)
+		if np.any(np.abs(expected_pks - measured_pks) > 1e-5):
+			self.fail("Expected peak heights are not equal to measured peak heights")
+		if np.any(np.abs(expected_t_pks - measured_t_pks) > 1e-5):
+			self.fail("Expected peak locations are not equal to measured peak locations")
+
 
 if __name__ == '__main__':
 	gr_unittest.run(qa_find_local_max, "qa_find_local_max.xml")
